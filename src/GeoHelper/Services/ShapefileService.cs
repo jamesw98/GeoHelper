c7fca239fa71103bbb57c01a785b5a27ed13e7c3
jamesw98 <xeekatar@gmail.com>
2024-02-03 00:04:06 -0500

    semi-working

---
+++ src/GeoHelper/Services/ShapefileService.cs
@@ -0,0 +1,40 @@
+﻿using GeoHelper.Models;
+using Microsoft.AspNetCore.Components.Forms;
+using NetTopologySuite.IO.Esri;
+using NetTopologySuite.IO.Esri.Dbf;
+using NetTopologySuite.IO.Esri.Shapefiles.Readers;
+
+namespace GeoHelper.Services;
+
+public class ShapefileService
+{
+    /// <summary>
+    /// Intentionally empty, for now.
+    /// </summary>
+    public ShapefileService()
+    {
+    }
+
+    // public async Task<string[]> ReadDbfFile(IBrowserFile file)
+    // {
+    //     if (file.Name.Split('.').Last().ToLower() != "dbf")
+    //     {
+    //         throw new ArgumentException("Unknown file type.");
+    //     }
+    //     
+    //     using var dbfReader = new DbfReader(file.OpenReadStream());
+    //     await dbfReader.Initialize();
+    //     
+    //     return dbfReader.FirstOrDefault()?.GetNames()
+    //            ?? throw new ArgumentException("Empty dbf file.");
+    // }
+    //
+    // public List<Polygon> ReadShapefile(IBrowserFile file)
+    // {
+    //     // Shapefile.OpenRead()
+    //     // var shapeReader = new ShapefilePolygonReader(file.OpenReadStream(), file.OpenReadStream());
+    //     //
+    //     // using var geos = Shapefile.ReadAllGeometries();
+    //     return new List<Polygon>();
+    // }
+}
--- src/GeoHelper/wwwroot/leaflet.js
+++ src/GeoHelper/wwwroot/leaflet.js
@@ -1,3 +1,13 @@
+export async function streamToJavaScript(streamRef) {
+    const data = await streamRef.arrayBuffer();
+    const geojson = await shp(data);
+    let count = 0;
+    geojson.features.forEach((feat) => {
+        let hex = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+        add_geojson_internal(feat, `test ${count++}`, hex)
+    });
+}
+
 const map = L.map('map').setView({ lon: -84.386330, lat: 33.753746  }, 5);
 
 let goesByName = {};
@@ -5,6 +15,28 @@ let goesByName = {};
 export function load_map() {
     // create the tile layer 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 }).addTo(map);
+}
+
+export function add_geojson_internal(geojson, name, hex) {
+    // create the geometry
+    let geo = L.geoJSON(geojson, {
+        style: function () {
+            return { color: hex }
+        }
+    });
+
+    geo.bindTooltip(name);
+    geo.on('mouseover', function(e){
+        geo.openTooltip();
+    });
+    geo.on('mouseout', function(e){
+        geo.closeTooltip();
+    });
+
+    // add it to the map
+    geo.addTo(map);
+
+    goesByName[name] = geo;
 }
 
 export function add_geojson(raw, name, hex) {
