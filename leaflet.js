f58f279e8d0e223efb1291cdd42c4f982a1c46b7
jamesw98 <xeekatar@gmail.com>
2024-02-25 15:22:51 +0000

    Deploying to gh-pages from  @ eb5c55686c24f9d099995a99e2b99e4d32b340fa 🚀

--- leaflet.js
+++ leaflet.js
@@ -1,3 +1,34 @@
+export async function importFile(streamRef) {
+    const data = await streamRef.arrayBuffer();
+    const geojson = await shp(data);
+    
+    let count = 0;
+    geojson.features.forEach((feat) => {
+        
+        let name = "";
+        if (feat.properties){
+            Object.keys(feat.properties).forEach((key) => {
+                name += `<b>${key}:</b> ${feat.properties[key]}<br>`;
+            });
+        }
+        
+        if (!name) {
+            name = `<b>Polygon:</b> ${count++}`;
+        }
+        
+        name += "<br><br>Right-click to remove."
+        
+        let hex = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+        add_geojson_internal(feat, name, hex)
+    });
+}
+
+export async function importDbf(streamRef) {
+    const data = await streamRef.arrayBuffer();
+    let dbf = shp.parseDbf(data);
+    return Object.keys(dbf[0]);
+}
+
 const map = L.map('map').setView({ lon: -84.386330, lat: 33.753746  }, 5);
 
 let goesByName = {};
@@ -5,6 +36,32 @@ let goesByName = {};
 export function load_map() {
     // create the tile layer 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 }).addTo(map);
+}
+
+export function add_geojson_internal(geojson, name, hex) {
+    // create the geometry
+    let geo = L.geoJSON(geojson, {
+        style: function () {
+            return { color: hex }
+        }
+    });
+
+    geo.bindTooltip(name);
+    geo.on('mouseover', function(e){
+        geo.openTooltip();
+    });
+    geo.on('mouseout', function(e){
+        geo.closeTooltip();
+    });
+    
+    geo.on("contextmenu", function(e) {
+       geo.remove()
+    });
+
+    // add it to the map
+    geo.addTo(map);
+
+    goesByName[name] = geo;
 }
 
 export function add_geojson(raw, name, hex) {
@@ -37,6 +94,4 @@ export function remove_geo(name) {
     goesByName[name].remove()
 }
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+
