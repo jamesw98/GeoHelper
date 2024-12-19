d69805c1020e1f6592432bb4929de76fcf4e0b11
jamesw98 <xeekatar@gmail.com>
2024-12-18 23:20:31 -0500

    added a class

---
+++ src/GeoHelper/Models/LeafletViewport.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GeoHelper.Models;
+
+public class LeafletViewport
+{
+    [JsonPropertyName("_southWest")]
+    public required SouthWest SouthWest { get; set; }
+
+    [JsonPropertyName("_northEast")]
+    public required NorthEast NorthEast { get; set; }
+
+    public override string ToString()
+    {
+        return $"NE: {NorthEast.Lat}, {NorthEast.Lng}\nSW: {SouthWest.Lat}, {SouthWest.Lng}";
+    }
+}
+
+public class NorthEast
+{
+    [JsonPropertyName("lat")]
+    public double Lat { get; set; }
+
+    [JsonPropertyName("lng")]
+    public double Lng { get; set; }
+}
+
+public class SouthWest
+{
+    [JsonPropertyName("lat")]
+    public double Lat { get; set; }
+
+    [JsonPropertyName("lng")]
+    public double Lng { get; set; }
+}
